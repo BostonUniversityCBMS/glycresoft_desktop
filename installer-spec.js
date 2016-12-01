1d5491f9bb870d9b53bf18b6fb007b9642d7b888
Joshua Klein <mobiusklein@gmail.com>
2016-12-01 14:27:20 -0500

    Updated backend

--- installer-spec.js
+++ installer-spec.js
@@ -3,8 +3,8 @@ var electronInstaller = require('electron-winstaller');
 resultPromise = electronInstaller.createWindowsInstaller({
     appDirectory: 'dist/win-unpacked',
     outputDirectory: 'dist/winstaller64',
-    authors: 'JK',
-    exe: 'glycresoft_electron.exe'
+    authors: 'Joshua Klein, Center for Biomedical Mass Spectrometry, Program for Bioinformatics, Boston University',
+    exe: 'GlycReSoft.exe'
   });
 
 resultPromise.then(() => console.log("It worked!"), (e) => console.log(`No dice: ${e.message}`));
