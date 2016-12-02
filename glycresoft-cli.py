c1846291c13585424d64b95dbbf496af69fc63f1
Joshua Klein <mobiusklein@gmail.com>
2016-12-02 10:57:06 -0500

    Moved PyInstaller workflow into repository and removed artifact

---
+++ glycresoft-cli.py
@@ -0,0 +1,18 @@
+import matplotlib
+try:
+    matplotlib.use("agg")
+except:
+    pass
+import multiprocessing
+from brainpy._c import composition, isotopic_constants, double_vector, isotopic_distribution
+from ms_peak_picker._c import double_vector
+import ms_deisotope
+from ms_deisotope._c import averagine, scoring, deconvoluter_base
+import glypy
+from glypy.plot import plot
+from glycan_profiling.cli.__main__ import main
+
+
+if __name__ == '__main__':
+    multiprocessing.freeze_support()
+    main()
