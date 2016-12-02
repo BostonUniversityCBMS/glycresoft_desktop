d3df6e0f11d407bb6ab01c3e0e81057b1a678676
Joshua Klein <mobiusklein@gmail.com>
2016-12-02 10:57:33 -0500

    Added new logo

--- installer-spec.js
+++ installer-spec.js
@@ -4,7 +4,8 @@ resultPromise = electronInstaller.createWindowsInstaller({
     appDirectory: 'dist/win-unpacked',
     outputDirectory: 'dist/winstaller64',
     authors: 'Joshua Klein, Center for Biomedical Mass Spectrometry, Program for Bioinformatics, Boston University',
-    exe: 'GlycReSoft.exe'
+    exe: 'GlycReSoft.exe',
+    iconUrl: "https://raw.githubusercontent.com/mobiusklein/glycresoft_desktop/master/img/logo.ico"
   });
 
 resultPromise.then(() => console.log("It worked!"), (e) => console.log(`No dice: ${e.message}`));
