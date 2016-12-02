a2986251f84143ffc71e4e8478019e47f8d7e52b
Joshua Klein <mobiusklein@gmail.com>
2016-12-02 08:56:03 -0500

    Added new logo

--- img/makelogo.sh
+++ img/makelogo.sh
@@ -1,1 +1,1 @@
-inkscape.com --export-png=logo.png --export-dpi=200 --export-background-opacity=0 --without-gui GlycReSoftLogo.svg
+inkscape.com --export-png=logo.png --export-dpi=200 --export-background-opacity=0 --without-gui logo.svg
