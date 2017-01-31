0e47eb284a32f85c17b188d96c3b3fed8c1fd34a
Joshua Klein <mobiusklein@gmail.com>
2017-01-31 16:00:44 -0500

    Updated

--- build-server-win64.sh
+++ build-server-win64.sh
@@ -1,5 +1,5 @@
 echo "Removing build artifacts"
 rm -rf pyinstaller-build/ server/win64/
 echo "Beginning build"
-python -m PyInstaller -c glycresoft-cli.py --exclude-module _tkinter --exclude-module PyQt4 --exclude-module IPython\
+python -m PyInstaller -c glycresoft-cli.py -D --exclude-module _tkinter --exclude-module PyQt4 --exclude-module IPython\
        --workpath pyinstaller-build --distpath server/win64 -i img/logo.ico
