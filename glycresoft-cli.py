7a00a8326f4b0163620f0b9419bf25191ec3f939
Joshua Klein <mobiusklein@gmail.com>
2017-07-10 16:45:25 -0400

    update dirty working copy

--- glycresoft-cli.py
+++ glycresoft-cli.py
@@ -1,8 +1,6 @@
+import sys
 import matplotlib
-try:
-    matplotlib.use("agg")
-except:
-    pass
+matplotlib.use("agg")
 import multiprocessing
 from brainpy._c import composition, isotopic_constants, double_vector, isotopic_distribution
 from ms_peak_picker._c import double_vector
@@ -13,8 +11,11 @@ import ms_deisotope
 from ms_deisotope._c import averagine, scoring, deconvoluter_base
 import glypy
 from glypy.plot import plot
+
+from glycan_profiling.cli.validators import strip_site_root
 from glycan_profiling.cli.__main__ import main
 
+sys.excepthook = strip_site_root
 
 if __name__ == '__main__':
     multiprocessing.freeze_support()
